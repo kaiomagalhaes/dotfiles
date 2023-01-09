6b6569b23e11ec0728ceb3c519acb7c7449c4c92
kaio magalhaes <me@kaiomagalhaes.com>
2023-01-09 12:56:06 -0500

    update setup

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -1,4 +1,15 @@
+upgrade
+
+xcode-select --install
+
+brew install docker
 brew install gh
 brew install slack
 brew install todoist
 brew install spotify
+brew install zoom
+brew install visual-studio-code
+brew install google-chrome
+brew install roam-research
+brew install microsoft-teams
+brew tap heroku/brew && brew install heroku
