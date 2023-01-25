817f0c92e032c0796fbe14a1c2d6701608b0bccd
Kaio Magalhães <me@kaiomagalhaes.com>
2023-01-25 09:52:08 -0300

    Update setup-new-machine.sh

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -2,7 +2,7 @@ upgrade
 
 xcode-select --install
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install docker
 brew install gh
 brew install slack
@@ -16,3 +16,4 @@ brew install microsoft-teams
 brew tap heroku/brew && brew install heroku
 brew install --cask iterm2
 brew install --cask alfred
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
